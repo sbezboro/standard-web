317fb60f198a6887d46a66cc025e04d6c7dea758
sbezboro <sergei.b.ru@gmail.com>
2013-04-04 00:18:21 -0700

    update server paths

--- query.sh
+++ query.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-. /home/sbezboro/standardsurvival-env/bin/activate; python /home/sbezboro/standard-web/query.py
+. /home/sbezboro/standard-web-env/bin/activate; python /home/sbezboro/standard-web/query.py
