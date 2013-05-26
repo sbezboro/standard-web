cb18ab0886fca7aacbb800d5c2f38e7168d6f8a0
sbezboro <sergei.b.ru@gmail.com>
2013-05-25 19:13:42 -0700

    show more hopefully helpful analytics

--- standardweb/views.py
+++ standardweb/views.py
@@ -61,26 +61,31 @@ def analytics(request, server_id=None):
     weeks = (datetime.utcnow() - earliest_date).days / 7
     
     for i in xrange(weeks + 1):
-        cohorts.append({'players': 0, 'active': [], 'inactive': 0, 'new': 0})
+        cohorts.append({'entrants': [], 'total_time_spent': 0, 'less_than_hour': 0, 'active': 0, 'returns': [], 'inactive': 0})
         
-    players = MinecraftPlayer.objects.filter(stats__server_id=server.id).values('username', 'stats__first_seen', 'stats__last_seen')
+    players = MinecraftPlayer.objects.filter(stats__server_id=server.id).values('username', 'stats__first_seen', 'stats__last_seen', 'stats__time_spent')
     
     for player in players:
         username = player['username']
         first_seen = player['stats__first_seen']
         last_seen = player['stats__last_seen']
+        time_spent = player['stats__time_spent']
         
         difference = first_seen - earliest_date
-        cohort = difference.days / 7
-        cohorts[cohort]['new'] += 1
+        entry_week = difference.days / 7
+        cohorts[entry_week]['entrants'].append(username)
+        cohorts[entry_week]['total_time_spent'] += time_spent
+        
+        if time_spent < 60:
+            cohorts[entry_week]['less_than_hour'] += 1
         
         difference = last_seen - earliest_date
         last_week = difference.days / 7
-        if last_week == weeks: #still here this week
-            cohorts[last_week]['active'].append(username)
-        else:
-            cohorts[last_week]['inactive'] += 1
         
+        if last_week == weeks: #still here now
+            cohorts[entry_week]['active'] += 1
+        elif last_week == entry_week: #only seen this entry_week
+            cohorts[entry_week]['inactive'] += 1
         '''
         if cohort < weeks:
             cohorts[cohort].setdefault('returns', [0] * (weeks - cohort))
@@ -92,6 +97,10 @@ def analytics(request, server_id=None):
                     if i == 0:
                         cohorts[cohort]['active'].append(player.username)
         '''
+    for cohort in cohorts:
+        if len(cohort['entrants']):
+            minutes = cohort['total_time_spent'] / len(cohort['entrants'])
+            cohort['average_time_spent'] = h.elapsed_time_string(minutes)
     
     return render_to_response('analytics.html', {
         'servers': Server.objects.all(),
@@ -128,8 +137,8 @@ def _get_player_graph_data(server, show_new_players=False, granularity=15, start
     start_date = start_date or end_date - timedelta(days = 7)
     
     statuses = ServerStatus.objects.filter(server=server,
-                                           timestamp__gt=start_date,
-                                           timestamp__lt=end_date).order_by('timestamp')
+                                           timestamp__gte=start_date,
+                                           timestamp__lte=end_date).order_by('timestamp')
     
     if show_new_players:
         new_players = PlayerStats.objects.filter(server=server,
@@ -192,14 +201,15 @@ def player_graph(request, server_id=None):
     return HttpResponse(json.dumps(graph_data), mimetype="application/json")
 
 
-def player_list(request):
+def player_list(request, server_id=None):
     stats = cache.get('minecraft-stats')
     
     if not stats:
         stats = {}
 
         try:
-            server = Server.objects.get(id=2)
+            server_id = int(server_id or 2)
+            server = Server.objects.get(id=server_id)
             
             server_status = api.get_server_status(server)
             
