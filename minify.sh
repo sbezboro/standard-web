10268ed406c0664745d92e390fc570b2e635a66f
sbezboro <sergei.b.ru@gmail.com>
2013-07-03 16:47:12 -0700

    include unminified jquery, adding minification script, removing extra scripts from pages

---
+++ minify.sh
@@ -0,0 +1,11 @@
+uglifyjs static/js/jquery-1.8.3.js\
+    static/js/jquery.flot.min.js\
+    static/js/jquery.placeholder.min.js\
+    static/js/jquery.timeago.min.js\
+    static/js/jquery.tipsy.min.js\
+    static/js/ZeroClipboard.min.js\
+    static/js/graph.js\
+    static/js/streams.js\
+    --source-map static/js/all.min.map\
+    --source-map-url all.min.map\
+    --output static/js/all.min.js
