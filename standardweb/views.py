106252764bd40ba1ff13262d9dbb99d275a1667e
sbezboro <sergei.b.ru@gmail.com>
2013-12-10 01:03:26 -0800

    remove 1.7.2 notice

--- standardweb/views.py
+++ standardweb/views.py
@@ -33,9 +33,6 @@ def index(request):
     news_post = news_topic.posts.filter(deleted=False).order_by('created')[0]
     comments = news_topic.posts.count() - 1
 
-    h.flash_success(request, 'The server has finally been upgraded to 1.7.2! Hopefully it stays this way assuming things are stable long-term.',
-                 title='1.7.2')
-
     if not h.mojang_status().session:
         h.flash_warning(request, 'Minecraft session servers are down! You won\'t be able to connect to this server until they are back up.',
                         title='Heads up!')
